e6ef9084b98332679d3f7501ffaba93f15654fa7
mohanjith <mohanjith@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-05-04 14:56:34 +0000

    Stable tag
    git-svn-id: https://plugins.svn.wordpress.org/chat/trunk@380778 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- chat.php
+++ chat.php
@@ -9,6 +9,7 @@ page by clicking on the new chat icon in your post/page editor.
  Author: S H Mohanjith (Incsub)
  WDP ID: 159
  Version: 1.0.3
+ Stable tag: 1.0.3
  Author URI: http://premium.wpmudev.org
 */
 /**
